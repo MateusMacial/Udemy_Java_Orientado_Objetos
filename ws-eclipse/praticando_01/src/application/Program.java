4921decad869c2c5bc3df382a65ff97d2b9204e5
Mateus Macial <mateus_macial@hotmail.com>
2021-05-21 22:23:21 -0300

    aula

--- ws-eclipse/praticando_01/src/application/Program.java
+++ ws-eclipse/praticando_01/src/application/Program.java
@@ -13,9 +13,14 @@ public class Program {
 		
 		Rectangle rectangle = new Rectangle();
 		
-		System.out.println("Enter de width value: ");
+		System.out.println("Enter the width and height value: ");
+		rectangle.width = sc.nextDouble();
+		rectangle.height = sc.nextDouble();
 		
 		
+		System.out.printf("Area %.2f%n",rectangle.area()); 
+		System.out.printf("Perimeter %.2f%n",rectangle.perimeter());
+		System.out.printf("Diagonal %.2f%n",rectangle.diagonal());
 		
 		sc.close();
 
